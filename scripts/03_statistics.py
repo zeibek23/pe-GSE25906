b0694f15db6762ecae239df57f6af0addc1cb9e3
Enes Zeybek <eczeneszeybek@gmail.com>
2026-09-08 11:37:32 +0100

    Add .gitignore; stop tracking data and results

---
+++ scripts/03_statistics.py
@@ -0,0 +1,66 @@
+import pandas as pd
+
+expression = pd.read_csv("results/01_expression.csv", index_col=0)
+diagnosis = pd.read_csv("results/01_diagnosis.csv", index_col=0).squeeze()
+
+print( expression.shape)
+print(expression.head(5))
+print(type(diagnosis))
+print(len(diagnosis))
+print(all(diagnosis.index == expression.columns))
+
+overall_mean = expression.mean(axis=1)
+expression_filter = expression[overall_mean > 7]
+print(expression_filter)
+
+from scipy import stats
+
+is_pe = diagnosis == "preeclamptic"
+group_pe = expression_filter.loc[:, is_pe.values]
+group_non = expression_filter.loc[:, (~is_pe).values]
+
+print(group_pe.shape, group_non.shape)
+
+tstat, pval = stats.ttest_ind(group_pe, group_non, axis=1, equal_var=False)
+print(len(pval))
+print(pval[:5])
+print(pd.isna(pval).sum())
+
+pval = pd.Series(pval, index=expression_filter.index)
+from statsmodels.stats.multitest import multipletests
+
+reject, padj, _, _ = multipletests(pval.values, alpha=0.05, method="fdr_bh")
+padj = pd.Series(padj, index=expression_filter.index)
+
+print ((pval < 0.05). sum())
+print ((padj < 0.05).sum())
+
+mean_pe = group_pe.mean(axis=1)
+mean_non = group_non.mean(axis=1)
+
+stats_df = pd.DataFrame({
+    "mean_PE": mean_pe,
+    "mean_nonPE": mean_non,
+    "log2FC": mean_pe-mean_non,
+    "pval": pval,
+    "padj": padj,
+})
+
+stats_df.to_csv("results/03_stats.csv")
+print(stats_df.shape)
+print(stats_df.head(10))
+print(stats_df.sort_values("padj").head(20))
+
+
+annotation = pd.read_csv("data/GPL6102.annot", sep="\t", skiprows=28, skipfooter=1, engine="python")
+columns_to_keep = ["ID", "Gene symbol"]
+annotation = annotation[columns_to_keep]
+final = stats_df.merge(annotation, left_index=True, right_on="ID", how="left")
+print(final.shape)
+print(final.head(5))
+
+top20 = final.sort_values("padj").head(20)
+print(top20[["ID", "Gene symbol", "log2FC", "pval", "padj"]])
+final.to_csv("results/03_stats_annotated.csv", index=False)
+print("stats_annotated have been saved inside the results folder!!")
+
---
+++ scripts/04_valcano.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+final = pd.read_csv("results/03_stats_annotated.csv")
+
+padj_safe = np.maximum(final["padj"], 1e-300)
+final["neglog10padj"] = -np.log10(padj_safe)
+
+sig = (final["padj"] < 0.05) & (final["log2FC"].abs()>0.5)
+
+plt.figure(figsize=(10,8))
+plt.scatter(final["log2FC"], final["neglog10padj"], s=4, c="grey", alpha=0.4)
+plt.scatter(final.loc[sig, "log2FC"], final.loc[sig, "neglog10padj"], s=6, c="red")
+
+for _, row in final.sort_values("padj").head(10).iterrows():
+    if pd.notna(row["Gene symbol"]):
+        plt.annotate(row["Gene symbol"],
+            (row["log2FC"], row["neglog10padj"]),
+            fontsize=7)
+plt.axhline(-np.log10(0.05), ls="--", lw=0.8)        
+plt.axvline(0.5, ls="--", lw=0.8)
+plt.axvline(-0.5, ls="--", lw=0.8)
+plt.xlabel("log2 fold change (Preeclamptic vs Control)")
+plt.ylabel("-log10 adjusted p-value")
+plt.title("GSE25906 Preeclamptic vs Control Analysis")
+plt.tight_layout()
+plt.savefig("results/04_volcano.png", dpi=1200)
+print("Figure saved")
+print(sig.sum())
